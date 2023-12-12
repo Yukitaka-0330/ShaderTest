3bcceda9a7df8982212955e36124b1b27be954a5
幸丘 叶内 <220209@jc-21.jp>
2023-12-12 16:35:19 +0900

    いろいろと変更した

--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -90,7 +90,5 @@ float4 PS(VS_OUT inData) : SV_Target
 		ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambentSource;
 	}
 
-   // return diffuse + ambient + specular;
-
-	return reflect;
+    return diffuse + ambient + specular;
 }
