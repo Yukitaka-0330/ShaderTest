977cb993de36dc1d67f46459ceb9b377d2552f0f
Kanauchi_Yukitaka <220209@jc-21.jp>
2024-01-09 16:34:05 +0900

    a

--- Toon.hlsl
+++ Toon.hlsl
@@ -81,43 +81,14 @@ float4 PS(VS_OUT inData) : SV_Target
 	float4 diffuse;
 	float4 ambient;
 	float4 NL = dot(inData.normal, normalize(lightPosition));
-	//float4 NL = saturate(dot(inData.normal, normalize(lightPosition)));
 
 	float4 reflect = normalize(2 * NL * inData.normal - normalize(lightPosition));
 	float4 specular = pow(saturate(dot(reflect, normalize(inData.eyev))), shiness) * specularColor;
-
-	//ŠK’²•ÏŠ·
-	/*float4 tonedColor;
-	if (inData.color.x < 1 / 3.0)
-	{
-		tonedColor = float4(0, 0, 0, 1.0);
-	}
-	else if (inData.color.x < 2 / 3.0)
-	{
-		tonedColor = float4(0.5, 0.5, 0.5, 1.0);
-	}
-	else
-	{
-		tonedColor = float4(1, 1, 1, 1);
-	}
-
-	return tonedColor;*/
-
-	/*float4 n1 = float4(1 / 4.0, 1 / 4.0, 1 / 4.0, 1);
-	float4 n2 = float4(2 / 4.0, 2 / 4.0, 2 / 4.0, 1);
-	float4 n3 = float4(3 / 4.0, 3 / 4.0, 3 / 4.0, 1);
-	float4 n4 = float4(4 / 4.0, 4 / 4.0, 4 / 4.0, 1);
-
-	float4 tI = 0.1 * step(n1, inData.color) + 0.3 * step(n2, inData.color)
-			  + 0.3 * step(n3, inData.color) + 0.4 * step(n4, inData.color);*/
 
 	float2 uv;
 	uv.x = abs(dot(inData.normal, normalize(inData.eyev)));
 	uv.y = abs(dot(inData.normal, normalize(inData.eyev)));
 	float4 tI = g_toon_texture.Sample(g_sampler, uv);
-	//return g_toon_texture.Sample(g_sampler, uv);
-
-	//return tI;
 
 	if (isTexture == false)
 	{
@@ -130,14 +101,14 @@ float4 PS(VS_OUT inData) : SV_Target
 		ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambentSource;
 	}
 
-	if (abs(dot(inData.normal,normalize(inData.eyev))) < 0.4f)
+	/*if (abs(dot(inData.normal,normalize(inData.eyev))) < 0.4f)
 	{
 		return float4(0, 0, 0, 1);
 	}
 	else
 	{
 		return float4 (1, 1, 1, 1);
-	}
+	}*/
 
 	return diffuse + ambient;
 
